25320394a6bc4d6495c6daea2a31ebae31c79b48
LvXiBo <2142526310@qq.com>
2023-03-20 12:00:21 +0800

    变更

--- src/Util/DBConnection.java
+++ src/Util/DBConnection.java
@@ -1,4 +1,44 @@
 package Util;
 
+import java.sql.*;
+
 public class DBConnection {
+    private static final String driveName = "com.mysql.cj.jdbc.Driver";
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/ware?serverTimezone=Asia/Shanghai";
+    private static final String username = "root";
+    private static final String password = "123456";
+
+    static {
+        try {
+            Class.forName(driveName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void close(ResultSet resultSet, Statement statement, Connection connection) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
